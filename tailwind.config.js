3ed4cbff1ee904243b63e9207459512d63672134
Sam Bowen <samhbowen@gmail.com>
2025-06-21 20:56:39 -0700

    Adds Tailwind CSS
    Configures Tailwind CSS for styling the application.
    
    This includes installing the necessary Tailwind CSS and PostCSS dependencies, configuring PostCSS to use Tailwind, updating the main CSS file to include Tailwind directives, and defining custom theme colors and fonts in the Tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ash: '#7F7F56',
+        moss: '#2A2E18',
+        fog: '#D2DCE2',
+        sand: '#D0CD8A',
+        forest: '#122620',
+      },
+      fontFamily: {
+        sans: ['"Neue Montreal"', 'ui-sans-serif', 'system-ui'],
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
-} 
+}
